1c43f1e21bafb4ab223b4763ef25cb3f8f55c747
manjeet-123 <manjeetsingh08484@gmail.com>
2020-10-20 15:13:48 +0530

    updating collection item of the shop page which in inside the front page

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import { Switch , Route } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.compoent';
 
 
-const HatsPage = ()=> (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
+
 
 function App() {
   return (
     <div>
    <Switch>
    <Route exact path='/' component={HomePage} />
-   <Route  path='/hats' component={HatsPage} />
+   <Route  path='/shop' component={ShopPage} />
    </Switch>
     </div>
   );
 }
 
 export default App;
+ 
